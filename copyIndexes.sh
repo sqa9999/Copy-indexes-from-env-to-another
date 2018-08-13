6d41219ababedea0a04bc6b3f7cb78510933160d
sqa9999 <sqa9999@users.noreply.github.com>
2018-08-13 15:06:15 -0500

    Update copyIndexes.sh

--- copyIndexes.sh
+++ copyIndexes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
   
-mongo [any params needed to connect to mongo] --quiet < printindex.js > out
+mongo [any params needed to connect to mongo] --quiet < getIndexes.js > out
 # on the destination mongo
 mongo [any params needed to connect to mongo] < out
 rm out
